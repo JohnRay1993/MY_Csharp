695fda5cee96b4c0193f7aec3395c7f875e50f5c
JohnRay <raschehan@gmail.com>
2018-10-17 17:28:31 +0300

    folder

--- Fractals/Fractals/Form1.Designer.cs
+++ Fractals/Fractals/Form1.Designer.cs
@@ -71,6 +71,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
--- Fractals/Fractals/Form1.cs
+++ Fractals/Fractals/Form1.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Controls;
+using System.IO;
 
 
 namespace Fractals
 {
     public partial class Form1 : Form
     {
-        Graphics g;
+        Bitmap bitmap;
+        Graphics g, g2;
         Brush brush;
         Pen pen;
-        v2 a, b, c, d, t, start, last;
+        v2 a, b, c, d, e2, t, start, last;
         int multiplicator = 100;
         int i = 0;
+        int count = 0;
+
+        string PNG_dir = "";
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
@@ -38,20 +40,33 @@ namespace Fractals
 
         private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
+            //Bitmap bitmap = new Bitmap(Convert.ToInt32(this.Width), Convert.ToInt32(this.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            //Graphics g = Graphics.FromImage(bitmap);
 
+            // Add drawing commands here
+            //g.Clear(Color.Black);
+
+            bitmap.Save(PNG_dir + "\test_" + count.ToString() + ".png", System.Drawing.Imaging.ImageFormat.Png);    //@"C:\Users\johndoe\
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             if (!timer1.Enabled)
             {
+                bitmap = new Bitmap(Convert.ToInt32(this.Width), Convert.ToInt32(this.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                PNG_dir = "session at " + DateTime.Now.Year.ToString() + "." + DateTime.Now.Month.ToString() + "." + DateTime.Now.Day.ToString() + "  " + DateTime.Now.Hour.ToString() + "." + DateTime.Now.Minute.ToString();
+                Directory.CreateDirectory(PNG_dir);
+
                 g = this.CreateGraphics();
-                brush = new SolidBrush(Color.Black);
+                g2 = Graphics.FromImage(bitmap);
+                brush = new SolidBrush(Color.Red);
 
                 pen = new Pen(Color.Black);
 
-                g.FillRectangle(brush, 0, 0, this.Width, this.Height);
-
+                //g.FillRectangle(brush, 0, 0, this.Width, this.Height);
+                g.Clear(Color.Black);
+                g2.Clear(Color.Black);
 
                 Random r = new Random();
                 a = new v2(); b = new v2(); c = new v2();
@@ -74,18 +89,20 @@ namespace Fractals
                 //c.x = this.Width - this.Width / 4;
                 //c.y = this.Height - this.Height / 4;
 
-                a = new v2(0, 100);
-                b = new v2(this.Width, 100);
+                a = new v2(0, 0);
+                b = new v2(this.Width, 0);
                 c = new v2(0, this.Height);
                 d = new v2(this.Width, this.Height);
-
-
+                
 
                 pen.Color = Color.White;
 
                 g.DrawRectangle(pen, a.x, a.y, 1, 1);
                 g.DrawRectangle(pen, b.x, b.y, 1, 1);
                 g.DrawRectangle(pen, c.x, c.y, 1, 1);
+                g2.DrawRectangle(pen, a.x, a.y, 1, 1);
+                g2.DrawRectangle(pen, b.x, b.y, 1, 1);
+                g2.DrawRectangle(pen, c.x, c.y, 1, 1);
 
 
                 int left, right, top, bottom;
@@ -108,7 +125,7 @@ namespace Fractals
                 start = new v2(r.Next(100, 300), r.Next(100, 400));
 
                 t = start;
-
+                
                 timer1.Enabled = true;
             }
             else
@@ -116,23 +133,104 @@ namespace Fractals
                 timer1.Enabled = false;
             }
             
+
+
+            //g = this.CreateGraphics();
+            //brush = new SolidBrush(Color.Black);
+
+            //pen = new Pen(Color.Black);
+
+            //g.FillRectangle(brush, 0, 0, this.Width, this.Height);
+
+            //pen = new Pen(Color.White);
+            ////y=100*[x/50]
+            ////y=50*[x/(100/[x/100])]
+            ////X' = -0.5*X -0.5*Y + 490 
+            ////Y' = 0.5*X -0.5*Y + 120 
+            //int h = this.Height, w = this.Width;
+            ////a = new v2(h, 0);
+            ////b = new v2(w / 2, 100);
+            ////c = new v2(w, h);
+            ////d = new v2(w / 2, h);
+            ////e2 = new v2(w, 100);
+
+            //a = new v2(h, 0);
+            //b = new v2(w / 2, 100);
+            //c = new v2(w, h);
+            //d = new v2(w / 2, h);
+            //e2 = new v2(w, 100);
+
+            //double y = 0;
+            //for (double x = 0; x < 1000; x++)
+            //{
+                
+
+
+
+
+            //    g.DrawRectangle(pen, (int)x, (int)y, 1, 1);
+            //}
+
+
+
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Random r = new Random();
 
-            
 
-            for (int j = 0; j < 1000; j++)
-            {
-                i = r.Next(1, 5);
-                //multiplicator = 100;// r.Next(1, 11);
-                if (multiplicator < 51)
-                    multiplicator += 1;
-                else
-                    multiplicator = 2;
+            //if (count < 10000)
+                for (int j = 0; j < 1000; j++)
+                {
+                    this.Text = count++.ToString();
+                    i = r.Next(1, 5);
+                    //multiplicator = 100;// r.Next(1, 11);
+                    /*if (multiplicator < 11)
+                        multiplicator = 50;
+                    else
+                        multiplicator -= 10;*/
+                    int q = 1;
+                    multiplicator = r.Next(q, r.Next(q, r.Next(q, r.Next(q, 51))));
 
+                    if (i == 1)
+                    {
+                        t.x += (a.x - t.x) / multiplicator;
+                        t.y += (a.y - t.y) / multiplicator;
+                    }
+                    if (i == 2)
+                    {
+                        t.x += (b.x - t.x) / multiplicator;
+                        t.y += (b.y - t.y) / multiplicator;
+                    }
+                    if (i == 3)
+                    {
+                        t.x += (c.x - t.x) / multiplicator;
+                        t.y += (c.y - t.y) / multiplicator;
+                    }
+                    if (i == 4)
+                    {
+                        t.x += (d.x - t.x) / multiplicator;
+                        t.y += (d.y - t.y) / multiplicator;
+                    }
+                    //if (t.x < 0) t.x *= -1;
+                    //if (t.y < 0) t.y *= -1;
+
+
+                    pen.Color = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+
+                    g.DrawRectangle(pen, t.x, t.y, 1, 1);
+                    g2.DrawRectangle(pen, t.x, t.y, 1, 1);
+                }
+            //else
+            //{
+            //    bitmap.Save("test.png", System.Drawing.Imaging.ImageFormat.Png);    //@"C:\Users\johndoe\
+            //}
+        }
+    }
+}
+/*
                 if (i == 1)
                 {
                     t.x += (a.x - t.x) / multiplicator;
@@ -153,15 +251,4 @@ namespace Fractals
                     t.x += (d.x - t.x) / multiplicator;
                     t.y += (d.y - t.y) / multiplicator;
                 }
-                //if (t.x < 0) t.x *= -1;
-                //if (t.y < 0) t.y *= -1;
-
-
-                pen.Color = Color.FromArgb(r.Next(100, 255), r.Next(100, 255), r.Next(100, 255));
-
-                g.DrawRectangle(pen, t.x, t.y, 1, 1);
-            }
-
-        }
-    }
-}
+*/
